de234e6c49b5056eb683b8246c2409cf42c40964
huangwanbin <wanbin_huang@qingsongchou.com>
2020-07-01 11:41:45 +0800

    预约日历-页面完成-交互未完

--- src/views/customer/exhibitor/appointment_calendar/index.vue
+++ src/views/customer/exhibitor/appointment_calendar/index.vue
@@ -7,9 +7,9 @@
       <p class="job_title">销售经理</p>
     </div>
     <div class="year_month">
-      <span class="left"></span>
-      <p class="month">2020.06</p>
-      <span class="right"></span>
+      <span class="left" @click="weekPre"></span>
+      <p class="month">{{ currentYear }}.{{ currentMonth }}</p>
+      <span class="right" @click="weekNext"></span>
      <van-field
       class="select_type"
       readonly
@@ -19,8 +19,39 @@
       @click="showPicker = true"
     />
     </div>
-    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
-      
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh" class="push_box">
+      <div class="date">
+        <!-- 星期 -->
+        <ul class="weekdays">
+          <li class="weekends">日</li>
+          <li>一</li>
+          <li>二</li>
+          <li>三</li>
+          <li>四</li>
+          <li>五</li>
+          <li class="weekends">六</li>
+        </ul>
+        <!-- 日期 -->
+        <ul class="days">
+          <li @click="pick(day)" v-for="(day, index) in days" :key="index">
+            <!--本月-->
+            <span v-if="day.getMonth()+1 != currentMonth" class="other-month">{{ day.getDate() }}</span>
+            <span v-else>
+            <!--今天-->
+            <span v-if="day.getFullYear() == new Date().getFullYear() && day.getMonth() == new Date().getMonth() && day.getDate() == new Date().getDate()" class="active">{{ day.getDate() }}</span>
+            <span v-else>{{ day.getDate() }}</span>
+            </span>
+            <p class="full" v-if="isFull">约满</p>
+          </li>
+        </ul>
+      </div>
+      <div class="date_content">
+        <div class="item" v-for="(item,index) in todayList" :key="index">
+          <p class="item_top">{{item.timeFrame}}{{item.interviewTime.substr(0,5)}}</p>
+          <p class="item_bottom">{{item.interviewStatus==2?'已预约':''}}</p>
+        </div>
+      </div>
+      <p class="refreshTip"><van-icon name="replay" class="replay_icon" />下拉刷新</p>
     </van-pull-refresh>
     <van-button class="bottom_button">预 约</van-button>
     <van-popup v-model="showPicker" position="bottom">
@@ -44,10 +75,177 @@ export default {
       showPicker:false,
       value1:'一周',
       columns: ['一周', '一天'],
+      currentYear: 1970,  // 年份
+      currentMonth: 1, // 月份
+      currentDay: 1,  // 日期
+      currentWeek: 1,  // 星期
+      days: [],
+      todayList:[],//当日预约情况
     };
   },
-  created() {},
+  computed:{
+    isFull(day){
+      console.log(day);
+      return 1
+    }
+  },
+  created() {
+    this.initData(null)
+    this.initTodayList()
+  },
   methods: {
+    initTodayList(){
+      let data = [{
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "08:00:00",
+            "interviewStatus": 1
+        }, {
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "08:30:00",
+            "interviewStatus": 2
+        }, {
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "09:00:00",
+            "interviewStatus": 0
+        }, {
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "09:30:00",
+            "interviewStatus": 0
+        }, {
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "10:00:00",
+            "interviewStatus": 2
+        }, {
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "10:30:00",
+            "interviewStatus": 1
+        }, {
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "11:00:00",
+            "interviewStatus": 0
+        }, {
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "11:30:00",
+            "interviewStatus": 0
+        }, {
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "13:00:00",
+            "interviewStatus": 0
+        }, {
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "13:30:00",
+            "interviewStatus": 0
+        }, {
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "14:00:00",
+            "interviewStatus": 0
+        },{
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "14:30:00",
+            "interviewStatus": 1
+        },{
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "15:00:00",
+            "interviewStatus": 1
+        },{
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "15:30:00",
+            "interviewStatus": 0
+        },{
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "16:00:00",
+            "interviewStatus": 2
+        },{
+            "userPreInterviewDetailId": "1276685523884294146",
+            "interviewTime": "16:30:00",
+            "interviewStatus": 1
+        },]
+        data.forEach((item,index)=>{          
+          if (item.interviewTime.substr(0,2)*1<12) {
+            item.timeFrame = '上午'
+          }else{
+            item.timeFrame = '下午'
+          }
+        })
+      this.todayList=data
+    },
+    formatDate (year, month, day) {
+      const y = year
+      let m = month
+      if (m < 10) m = `0${m}`
+      let d = day
+      if (d < 10) d = `0${d}`
+      return `${y}-${m}-${d}`
+    },
+   initData (cur) {
+    let date = ''
+    if (cur) {
+     date = new Date(cur)
+    } else {
+     date = new Date()
+    }
+    this.currentDay = date.getDate()     // 今日日期 几号
+    this.currentYear = date.getFullYear()    // 当前年份
+    this.currentMonth = date.getMonth() + 1  // 当前月份
+    this.currentWeek = date.getDay() // 1...6,0  // 星期几
+    if (this.currentWeek === 0) {
+     this.currentWeek = 7
+    }
+    const str = this.formatDate(this.currentYear, this.currentMonth, this.currentDay)// 今日日期 年-月-日
+    this.days.length = 0
+    // 今天是周日，放在第一行第7个位置，前面6个 这里默认显示一周，如果需要显示一个月，则第二个循环为 i<= 35- this.currentWeek
+    /* eslint-disabled */
+    for (let i = this.currentWeek - 1; i >= 0; i -= 1) {
+     const d = new Date(str)
+     d.setDate(d.getDate() - i-1)
+      // console.log(y:" + d.getDate())
+     this.days.push(d)
+     console.log(i,d);
+     
+    }
+    for (let i = 1; i <= 7 - this.currentWeek; i += 1) {
+     const d = new Date(str)
+     d.setDate(d.getDate() + i-1)
+     this.days.push(d)
+
+      console.log(i,d);
+         
+    }
+   },
+
+   // 上个星期
+   weekPre () {
+    const d = this.days[0]  // 如果当期日期是7号或者小于7号
+    d.setDate(d.getDate() - 7)
+    this.initData(d)
+   },
+
+   // 下个星期
+   weekNext () {
+    const d = this.days[6]  // 如果当期日期是7号或者小于7号
+    d.setDate(d.getDate() + 7)
+    this.initData(d)
+   },
+
+   // 上一個月  传入当前年份和月份
+   pickPre (year, month) {
+    const d = new Date(this.formatDate(year, month, 1))
+    d.setDate(0)
+    this.initData(this.formatDate(d.getFullYear(), d.getMonth() + 1, 1))
+   },
+
+
+   // 下一個月  传入当前年份和月份
+   pickNext (year, month) {
+    const d = new Date(this.formatDate(year, month, 1))
+    d.setDate(35)
+    this.initData(this.formatDate(d.getFullYear(), d.getMonth() + 1, 1))
+   },
+
+   // 当前选择日期
+   pick (date) {
+    console.log(this.formatDate(date.getFullYear(), date.getMonth() + 1, date.getDate()))
+   },
     onConfirm(value) {
       this.value1 = value;
       this.showPicker = false;
@@ -55,7 +253,8 @@ export default {
     onRefresh() {
       setTimeout(() => {
         this.isLoading = false;
-        alert(1)
+        console.log('刷新');
+        this.initTodayList()
       }, 1000);
     },
   }
@@ -65,6 +264,7 @@ export default {
 .appointment_calendar {
   background-color: #fff;
   .top_header{
+    margin-bottom: 0.48rem;
     transform: translateY(-0.82rem);
     width:7.5rem;
     height:3.68rem;
@@ -110,9 +310,11 @@ export default {
         background:linear-gradient(180deg,rgba(248,213,126,1) 0%,rgba(255,255,255,1) 100%);
   }
   .year_month{
+    transform: translateY(-0.82rem);
     display: flex;
     padding:0 0 0 1.08rem;
     align-items: center;
+    margin-bottom: 0.56rem;
     .left{
       margin-top: 0.01rem;
       display: block;
@@ -123,7 +325,6 @@ export default {
       transform: rotate(45deg);
     }
     .month{
-      width:1.24rem;
       height:0.4rem;
       font-size:0.32rem;
       font-family:"AlibabaPuHuiTiR";
@@ -161,7 +362,55 @@ export default {
       transform: rotate(135deg);
     }
   }
+  .push_box{
+    transform: translateY(-0.82rem);
+    .date_content{
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      align-items: center;
+      margin: 0.44rem auto 0;
+      width:6.96rem;
+      height:6.02rem;
+      background:rgba(255,255,255,1);
+      box-shadow:0rem 0.1rem 0.26rem 0rem rgba(223,227,233,0.51);
+      border-radius:0.1rem;
+      // padding: 0.34rem  0.26rem;
+    }
+    .date_content:nth-child(4){
+      margin: 0;
+    }
+    .item{
+      text-align: center;
+      // flex: 25%;
+      width:1.48rem;
+      height:0.94rem;
+      border-radius:0.1rem;
+      border:0.02rem solid rgba(248,213,126,1);
+      font-size: 0.26rem;
+      .item_top{
+        color: #313437;
+      }
+      .item_bottom{
+        color: #9DA1A6;
+      }
+    }
+    .refreshTip{
+      margin-top: 0.26rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height:0.34rem;
+      font-size:0.24rem;
+      font-family:"AlibabaPuHuiTiR";
+      color:rgba(74,152,247,1);
+      line-height:0.34rem;
+      .replay_icon{
+      }
+    }
+  }
   .bottom_button{
+    transform: translateY(-0.34rem);
     display: block;
     margin: 0 auto;
     width:6.96rem;
@@ -170,6 +419,65 @@ export default {
     border-radius:0.16rem;
   }
 }
+.date {
+  color: #333;
+
+  .month {
+   text-align: center;
+  }
+
+  .weekdays {
+   display: flex;
+   li {
+     color: #C8CCD3;
+    flex: 1;
+    text-align: center;
+   }
+   .weekends{
+     color: #F4B860;
+   }
+  }
+
+  .days {
+   display: flex;
+
+   li {
+    flex: 1;
+    text-align: center;
+    span{
+     display: inline-block;
+    font-size:0.26rem;
+    font-family:"AlibabaPuHuiTiR";
+    color: #313437;
+    line-height:0.56rem;
+     width:0.56rem;
+    height:0.56rem;
+    }
+    .active {
+     display: inline-block;
+    font-size:0.26rem;
+    font-family:"AlibabaPuHuiTiR";
+    color:rgba(49,52,55,1);
+    line-height:0.56rem;
+     width:0.56rem;
+    height:0.56rem;
+    background:rgba(248,213,126,1);
+    border-radius:0.16rem;
+    }
+    .full{
+      margin: 0 auto;
+      width: 0.4rem;
+      height: 0.28rem;
+      font-size:0.18rem;
+      color:#9DA1A6;
+      border: 0.01rem dotted #000;
+    }
+    .other-month {
+     color: #9DA1A6;
+    }
+   }
+  }
+ }
 /deep/ .van-field__control{
     height:0.4rem;
     font-size:0.24rem;

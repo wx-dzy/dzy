b76dec01bab7dd79d0c13b7061f44962eba367bd
ff <361204134@qq.com>
2020-07-16 02:42:38 +0800

    产品列表over

--- src/views/customer/products/products_details.vue
+++ src/views/customer/products/products_details.vue
@@ -20,7 +20,7 @@
       <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
         <van-swipe-item>
           <img
-            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1594846054205&di=8684beb593908fccfb3a9284e2046257&imgtype=0&src=http%3A%2F%2Fa0.att.hudong.com%2F56%2F12%2F01300000164151121576126282411.jpg"
+            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1594848577976&di=a9a8029834d73c872a5436a5311db2c8&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201309%2F03%2F20130903141830_Q4Wuc.jpeg"
             alt
           />
         </van-swipe-item>

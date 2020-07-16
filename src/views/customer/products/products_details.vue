bedf4a15838fac5b41943a61d61338889e5d201f
ff <361204134@qq.com>
2020-07-16 10:32:22 +0800

    产品列表over

--- src/views/customer/products/products_details.vue
+++ src/views/customer/products/products_details.vue
@@ -1,4 +1,4 @@
-// 企业产品目录-产品详情  getGoodDetailById
+// 企业产品目录-产品详情  http://localhost:9000/products_details?goodsId=1275702956557078530
 <template>
   <div class="products_details">
     <!-- 下拉刷新 -->
@@ -17,39 +17,41 @@
         </van-swipe-item>
       </van-swipe>
 
-      <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-        <van-swipe-item>
-          <img
-            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1594848577976&di=a9a8029834d73c872a5436a5311db2c8&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201309%2F03%2F20130903141830_Q4Wuc.jpeg"
-            alt
-          />
-        </van-swipe-item>
-      </van-swipe>
-
       <div class="contwrap">
         <van-row class="head border pad22">
           <h2 class="title">{{ detail.goodsBaseInfo.goodsName}}</h2>
 
           <van-col span="19">
-            <p>品 牌：宝矿力水特</p>
-            <p>订货号：A2323</p>
+            <p>品 牌：{{ detail.goodsBaseInfo.brandName }}</p>
+            <p>订货号：{{ detail.goodsBaseInfo.orderNo }}</p>
           </van-col>
-          <van-col span="5" class>
-            <van-button round size="small" color="#F8D57E">分享</van-button>
+          <van-col span="5" class="shareBtn text-right">
+            <van-button round size="small" color="#F8D57E">
+              分享1</van-button>
           </van-col>
         </van-row>
 
-        <van-row class="border pad22">
+        <van-row v-if="detail.goodsSpecificationList.length" class="border pad22">
           <h2 class="title">参数</h2>
-          <van-col>商品名称 宝矿力水特（POCARI</van-col>
-          <van-col>商品名称 宝矿力水特（POCARI</van-col>
-          <van-col>商品名称 宝矿力水特（POCARI</van-col>
+          <van-row v-for="(item,index) in detail.goodsSpecificationList" :key="index">
+            <van-col span="6">{{ item.name}}</van-col>
+            <van-col span="18">{{ item.values}}</van-col>
+          </van-row>
         </van-row>
-        <van-row class="border pad22">
+
+        <van-row class="border pad22" v-if="detail.goodsIntroductionList.length">
           <h2 class="title">产品详情</h2>
-          <van-col>商品名称 宝矿力水特（POCARI</van-col>
-          <van-col>商品名称 宝矿力水特（POCARI</van-col>
-          <van-col>商品名称 宝矿力水特（POCARI</van-col>
+          <van-col v-for="(item,index) in detail.goodsIntroductionList" :key="index">
+            <Video-Demo
+              v-if="item.videoUrl"
+              :_id="item.id"
+              :src="item.videoUrl"
+              :playVideoId.sync="playVideoId"
+              style="width: 100%;"
+              class="zoom"
+            />
+            <img v-if="item.mediaUrl" :src="item.mediaUrl" alt class="itemImg" />
+          </van-col>
         </van-row>
 
         <van-goods-action>
@@ -67,21 +69,27 @@
 import { util } from "@/utils";
 import { mapGetters } from "vuex";
 import * as Api from "@/api/customer/products";
+import VideoDemo from "@/components/customer/videoPlay/index.vue";
 // import visitingCard from "@/components/customer/visitingCard.vue";
 
 export default {
   name: "products_details",
   components: {
-    // 名片
-    // visitingCard
+    VideoDemo
   },
   data() {
     return {
+      //
+      playVideoId: "",
       // 商品di
       goodsId: "",
       detail: {
         // 轮播
-        goodsImagesList: []
+        goodsImagesList: [],
+        // 参数
+        goodsSpecificationList: [],
+        // 产品详情图片列表
+        goodsIntroductionList: []
       },
       pageSize: 10,
       pageNum: 1,

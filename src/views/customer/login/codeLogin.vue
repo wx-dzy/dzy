f80c2e5af59d8ae6ec1d7a4ccfc771f92c9d7ee5
赵亚 <1980674844@qq.com>
2020-07-30 22:06:15 +0800

    .

--- src/views/customer/login/codeLogin.vue
+++ src/views/customer/login/codeLogin.vue
@@ -316,7 +316,7 @@ export default {
 
             if (code == null || code == '') {
                 window.location.href =
-                    'http://www.dzy315.com/get-weixin-code.html?appid=wxc7ed228b39eec84c&scope=snsapi_base&state=123&redirect_uri=http://192.168.31.72:9000/&response_type=code'
+                    'http://www.dzy315.com/get-weixin-code.html?appid=wxc7ed228b39eec84c&scope=snsapi_base&state=123&redirect_uri=http://192.168.1.117:9000/&response_type=code'
 
                 // window.location.href =
                 //     'http://www.dzy315.com/get-weixin-code.html?appid=wxc7ed228b39eec84c&scope=snsapi_base&state=123&redirect_uri=http://www.dzy315.com/codeLogin&response_type=code'

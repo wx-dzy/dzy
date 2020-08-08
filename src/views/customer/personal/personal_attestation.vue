cbc0c8c4bb7b18890dabc03eb66135231699c888
cxy <2437657482@qq.com>
2020-08-08 17:43:27 +0800

    debug关

--- src/views/customer/personal/personal_attestation.vue
+++ src/views/customer/personal/personal_attestation.vue
@@ -101,7 +101,7 @@ export default {
                     const { code, data, msg, total } = res
                     if (code == 200) {
                         wx.config({
-                            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                             appId: data.appId, // 必填，公众号的唯一标识
                             timestamp: data.timestamp, // 必填，生成签名的时间戳
                             nonceStr: data.nonceStr, // 必填，生成签名的随机串
@@ -133,7 +133,7 @@ export default {
                 sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
                 success: function (res) {
                     // console.log('选择图片', res)
-                    var localIds = res.localIds // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    var localIds = res.localIds[0] // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
                     _this.upedImg = localIds
                     wx.uploadImage({
                         localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得

dbabee52e6d2d8107a57ebd080e670723c6a5a38
赵亚 <1980674844@qq.com>
2020-08-09 19:34:35 +0800

    修改

--- src/views/customer/login/codeLogin.vue
+++ src/views/customer/login/codeLogin.vue
@@ -324,7 +324,6 @@ export default {
                 //     'http://www.dzy315.com/get-weixin-code.html?appid=wxc7ed228b39eec84c&scope=snsapi_base&state=123&redirect_uri=http://192.168.1.112:9000/&response_type=code'
                 // window.location.href =
                 //     'http://www.dzy315.com/get-weixin-code.html?appid=wxc7ed228b39eec84c&scope=snsapi_base&state=123&redirect_uri=http://192.168.31.72:9000/&response_type=code'
-
                 window.location.href =
                     'http://www.dzy315.com/get-weixin-code.html?appid=wxc7ed228b39eec84c&scope=snsapi_base&state=123&redirect_uri=http://www.dzy315.com/codeLogin&response_type=code'
             } else {

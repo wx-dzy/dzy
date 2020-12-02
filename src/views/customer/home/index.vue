9d654d890580850190110517edb4c3fee41ad93b
ff <361204134@qq.com>
2020-12-02 11:45:57 +0800

    保存主页

--- src/views/customer/home/index.vue
+++ src/views/customer/home/index.vue
@@ -9,8 +9,13 @@
           <i>帮你放大招！</i>
         </h2>
 
-        <van-swipe v-if="banner.length" class="my-swipe" :autoplay="3000" indicator-color="white">
-          <van-swipe-item v-for="(image, index)  in banner" :key="index">
+        <van-swipe
+          v-if="banner.length"
+          class="my-swipe"
+          :autoplay="3000"
+          indicator-color="white"
+        >
+          <van-swipe-item v-for="(image, index) in banner" :key="index">
             <!-- <router-link :to="{'path':'personal'}"> -->
             <img :src="image.mediaUrl" alt />
             <!-- </router-link> -->
@@ -22,40 +27,58 @@
       <div v-if="specifyList.length">
         <div class="specifyWrap">
           <h3 class="title">指定参加</h3>
-          <ul class="specifyList clearfix" :style="{width: (4.62 *  specifyList.length) + 'rem'}">
+          <ul
+            class="specifyList clearfix"
+            :style="{ width: 4.62 * specifyList.length + 'rem' }"
+          >
             <li
-              v-for="(item, index)  in specifyList"
+              v-for="(item, index) in specifyList"
               :key="index"
               class="contentItem"
-              :class="index+1 == specifyList.length ? 'margin0' : ''"
+              :class="index + 1 == specifyList.length ? 'margin0' : ''"
               @click="handleLook(item)"
             >
-              <img v-if="!item.videoUrl" :src="item.mediaUrl" alt class="itemImg" />
+              <img
+                v-if="!item.videoUrl"
+                :src="item.mediaUrl"
+                alt
+                class="itemImg"
+              />
               <div v-else class="itemImg">
                 <Video-Demo
                   :_id="item.id"
                   :src="item.videoUrl"
                   :bannerIMG="item.mediaUrl"
                   :playVideoId.sync="playVideoId"
-                  style="width: 100%;"
+                  style="width: 100%"
                   class="zoom"
                 />
               </div>
               <ol class="cont">
                 <li class="tit">
                   <span class="type">{{ item.tagName }}</span>
-                  {{item.showName}}
+                  {{ item.showName }}
                 </li>
                 <!-- <li class="timer">{{ item.planStartDate.replace('-', ".") }} - {{ item.planEndDate.replace(/-/, ".") }}</li> -->
-                <li class="timer">{{ item.planStartDate }} - {{ item.planEndDate }}</li>
+                <li class="timer">
+                  {{ item.planStartDate }} - {{ item.planEndDate }}
+                </li>
                 <li class="clearfix">
-                  <img :src="item.logo" alt="logo" class="logo" />
-                  <i>{{ item.enterpriseName }}</i>
+                  <!-- <img :src="item.logo" alt="logo" class="logo" />
+                  <i>{{ item.enterpriseName }}</i> -->
                   <p class="statusWrap">
-                    <span
-                      class="status"
-                    >{{ item.showFormat == 1 ? '仅线上' : item.showFormat == 2 ? '仅线下' : item.showFormat == 3 ? '全包括' : ''}}</span>
-                    <span v-show="item.specifyStatus == 1" class="status">企业指定</span>
+                    <span class="status">{{
+                      item.showFormat == 1
+                        ? "仅线上"
+                        : item.showFormat == 2
+                        ? "仅线下"
+                        : item.showFormat == 3
+                        ? "全包括"
+                        : ""
+                    }}</span>
+                    <span v-show="item.specifyStatus == 1" class="status"
+                      >企业指定</span
+                    >
                   </p>
                   <!-- <van-button round type="default" size="small" style="float:right;">了解更多</van-button> -->
                 </li>
@@ -79,39 +102,68 @@
           class="contentList"
         >
           <van-cell
-            v-for="(item, index)  in listData"
+            v-for="(item, index) in listData"
             :key="index"
             class="contentItem"
-            :class="index+1 == listData.length ? 'margin0' : ''"
+            :class="index + 1 == listData.length ? 'margin0' : ''"
             @click="handleLook(item)"
           >
-            <img v-if="!item.videoUrl" :src="item.mediaUrl" alt class="itemImg" />
+            <img
+              v-if="!item.videoUrl"
+              :src="item.mediaUrl"
+              alt
+              class="itemImg"
+            />
             <div v-else class="itemImg">
               <Video-Demo
                 :_id="item.id"
                 :src="item.videoUrl"
                 :bannerIMG="item.mediaUrl"
                 :playVideoId.sync="playVideoId"
-                style="width: 100%;"
+                style="width: 100%"
               />
             </div>
 
             <ol class="cont">
               <li class="tit">
                 <span class="type">{{ item.tagName }}</span>
-                {{item.showName}}
+                {{ item.showName }}
               </li>
-              <li class="timer">{{ item.planStartDate }} - {{ item.planEndDate }}</li>
-              <li class="clearfix">
+              <li class="timer">
+                {{ item.planStartDate }} - {{ item.planEndDate }}
+                <p class="statusWrap">
+                  <span class="status">{{
+                    item.showFormat == 1
+                      ? "仅线上"
+                      : item.showFormat == 2
+                      ? "仅线下"
+                      : item.showFormat == 3
+                      ? "全包括"
+                      : ""
+                  }}</span>
+                  <span v-show="item.specifyStatus == 1" class="status"
+                    >企业指定</span
+                  >
+                </p>
+              </li>
+              <!-- <li class="clearfix">
                 <img :src="item.logo" alt="logo" class="logo" />
                 <i>{{ item.enterpriseName }}</i>
                 <p class="statusWrap">
-                  <span
-                    class="status"
-                  >{{ item.showFormat == 1 ? '仅线上' : item.showFormat == 2 ? '仅线下' : item.showFormat == 3 ? '全包括' : ''}}</span>
-                  <span v-show="item.specifyStatus == 1" class="status">企业指定</span>
+                  <span class="status">{{
+                    item.showFormat == 1
+                      ? "仅线上"
+                      : item.showFormat == 2
+                      ? "仅线下"
+                      : item.showFormat == 3
+                      ? "全包括"
+                      : ""
+                  }}</span>
+                  <span v-show="item.specifyStatus == 1" class="status"
+                    >企业指定</span
+                  >
                 </p>
-              </li>
+              </li> -->
             </ol>
           </van-cell>
         </van-list>
@@ -141,7 +193,7 @@ export default {
   name: "home",
   components: {
     VideoDemo, // 播放
-    footerNav
+    footerNav,
   },
   data() {
     return {
@@ -163,7 +215,7 @@ export default {
       finished: false,
       refreshing: false,
       pageSize: 10,
-      pageNum: 1
+      pageNum: 1,
     };
   },
 
@@ -181,7 +233,7 @@ export default {
     // }
   },
 
-   beforeRouteLeave(to, from, next) {
+  beforeRouteLeave(to, from, next) {
     // console.log(to)
     // return
     // 主页禁止返回
@@ -195,14 +247,14 @@ export default {
     // 获取轮播图
     handleGetBanner() {
       Api.getHomeMediaList()
-        .then(res => {
+        .then((res) => {
           let { code, msg, data, total } = res;
           if (code == 200) {
             this.banner = data.mediaList;
             this.specifyList = data.specifyList;
           }
         })
-        .catch(err => {
+        .catch((err) => {
           this.banner = [];
           this.specifyList = [];
         });
@@ -214,7 +266,7 @@ export default {
       this.pageNum = 1;
       let params = {
         pageNum: this.pageNum, // 页数
-        pageSize: this.pageSize // 每页几条数据
+        pageSize: this.pageSize, // 每页几条数据
       };
       // console.log(params,'onSearch')
       this.onsubmt(params);
@@ -225,7 +277,7 @@ export default {
       this.pageNum++;
       let params = {
         pageNum: this.pageNum, // 页数
-        pageSize: this.pageSize // 每页几条数据
+        pageSize: this.pageSize, // 每页几条数据
       };
       // console.log(params,'onLoad')
       this.onsubmt(params);
@@ -237,7 +289,7 @@ export default {
       this.pageNum = 1;
       let params = {
         pageNum: this.pageNum, // 页数
-        pageSize: this.pageSize // 每页几条数据
+        pageSize: this.pageSize, // 每页几条数据
       };
       this.onsubmt(params, 1);
     },
@@ -246,7 +298,7 @@ export default {
     onsubmt(params, statu) {
       let status = statu ? statu : 2; // 默认正常请求
       Api.getHomePage(params)
-        .then(res => {
+        .then((res) => {
           let { code, msg, data, total } = res;
           // 加载状态结束
           this.loading = false;
@@ -257,7 +309,7 @@ export default {
               this.listData = data;
               this.$toast("刷新成功");
             } else {
-              data.forEach(element => {
+              data.forEach((element) => {
                 this.listData.push(element);
               });
             }
@@ -267,7 +319,7 @@ export default {
             }
           }
         })
-        .catch(err => {
+        .catch((err) => {
           // 上拉刷新
           this.refreshing = false;
           console.log(err, "err");
@@ -280,16 +332,14 @@ export default {
         name: "home_details",
         query: {
           // 企业id
-          id: row.id
+          id: row.id,
           // title: row.enterpriseName
-        }
+        },
       });
-    }
+    },
   },
 
   computed: {},
-
- 
 
   mounted() {
     this.$nextTick().then(() => {
@@ -297,7 +347,7 @@ export default {
         window.scrollTo(0, 0);
       }, 0);
     });
-  }
+  },
 };
 </script>
 <style lang="scss">
